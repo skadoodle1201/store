88adac34892bfd6af56716468c47a1e497a00a5f
Tanish chaurasia <59193918+skadoodle1201@users.noreply.github.com>
2021-10-12 07:28:23 +0530

    11/10/21

--- app.js
+++ app.js
@@ -3,6 +3,70 @@ const app = express();
 
 
 
+
+
+/* const graphql,{GraphQLObjectType,GraphQLSchema,GraphQLInt,GraphQLString,GraphQLList, GraphQLInt} = require('graphql');
+const {graphqlHTTP}  = require('express-graphql');
+
+
+
+const UserType = new GraphQLObjectType ({
+  name : "store",
+  fields: () => ({
+    id : {type: GraphQLInt},
+    name  : { type : GraphQLString},
+    address  : { type : GraphQLString},
+    number  : { type : GraphQLInt},
+    email  : { type : GraphQLString}
+  })
+})
+
+
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields:{
+    getAllUsers : {
+      type: new GraphQLList(UserType),
+      args: {id : { type: GraphQLInt}},
+      resolve(parent,args){
+        return userData
+      }
+    }
+  }
+})
+const Mutation  = new GraphQLObjectType({
+  name: "Mutation",
+  fields:{
+    createUser : {
+      type: UserType,
+      args: {
+        name  : { type : GraphQLString},
+        address  : { type : GraphQLString},
+        number  : { type : GraphQLInt},
+        email  : { type : GraphQLString}
+      } ,
+      resolve(parent,args){
+
+      }
+    }
+  }
+})
+
+const schema  = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation
+})
+
+
+app.use('/graphql' , graphqlHTTP({
+  schema ,
+  graphiql: true
+}))
+ */
+
+
+
 app.get('/',(req,res)=>{
   res.send('HOME');
 })
